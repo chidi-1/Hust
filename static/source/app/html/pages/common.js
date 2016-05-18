579169013ed6fba97b2a436bd26bafabb169a60f
chidi_ <bev-olga@yandex.ru>
2016-05-18 04:28:25 +0400

    Новая верстка, 7 страниц. БЕЗ правок по js, правки по стилям: футер прибит к низу страницы, стили для заголовка и списков сделаны общими.

--- static/source/app/html/pages/common.js
+++ static/source/app/html/pages/common.js
@@ -123,6 +123,21 @@ $(document).ready(function (){
         });
     }
 
+    // аккордеон
+    if($(".accordeon").length) {
+        $('.acc_container').hide();
+        $('.acc_trigger:first').addClass('active').next().show();
+
+        //On Click
+        $('.acc_trigger').click(function () {
+            if ($(this).next().is(':hidden')) {
+                $('.acc_trigger').removeClass('active').next().slideUp();
+                $(this).toggleClass('active').next().slideDown();
+            }
+            return false;
+        });
+    }
+
     // ДЕЙСТВИЯ
 
 	$(".search form").hover(function () {
